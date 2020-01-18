05a124c2d2f6fd2d5b985f17c06799146cc9659e
Timofey-Tylik <timofey.tylik@gmail.com>
2020-01-18 16:04:08 -0500

    Implemented Rev Robotics Color Sensor V3 for FRC 2020

---
+++ src/main/java/frc/robot/utils/ColorSensor.java
@@ -0,0 +1,69 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.utils;
+
+import com.revrobotics.ColorMatch;
+import com.revrobotics.ColorMatchResult;
+import com.revrobotics.ColorSensorV3;
+
+import edu.wpi.first.wpilibj.I2C;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.util.Color;
+
+/**
+ * Add your docs here.
+ */
+public class ColorSensor {
+    public enum ColorValue {RED, YELLOW, GREEN, BLUE, UNKOWN};
+
+    //Accurate measurments for up to 2.5 inches
+    private static final Color BLUE_TARGET = ColorMatch.makeColor(0.135, 0.461, 0.404);
+    private static final Color GREEN_TARGET = ColorMatch.makeColor(0.197, 0.561, 0.240);
+    private static final Color RED_TARGET = ColorMatch.makeColor(0.504, 0.353, 0.144);
+    private static final Color YELLOW_TARGET = ColorMatch.makeColor(0.361, 0.524, 0.113);
+
+    private I2C.Port sensorPort;
+    private ColorSensorV3 colorSensor;
+    private ColorMatch m_colorMatcher;
+
+    public ColorSensor(I2C.Port sensorPort){
+        this.sensorPort = sensorPort;
+        colorSensor = new ColorSensorV3(sensorPort);
+        m_colorMatcher = new ColorMatch();
+
+        m_colorMatcher.addColorMatch(BLUE_TARGET);
+        m_colorMatcher.addColorMatch(GREEN_TARGET);
+        m_colorMatcher.addColorMatch(RED_TARGET);
+        m_colorMatcher.addColorMatch(YELLOW_TARGET);
+    }
+    
+    //Checks if the color is Red, Blue, Green, Yellow, or Unknown
+    public ColorValue getColor(){
+        Color detectedColor = colorSensor.getColor();
+        SmartDashboard.putNumber("Red", detectedColor.red);
+        SmartDashboard.putNumber("Green", detectedColor.green);
+        SmartDashboard.putNumber("Blue", detectedColor.blue);
+        
+        ColorMatchResult match = m_colorMatcher.matchColor(detectedColor);
+
+        if (match == null){
+            return ColorValue.UNKOWN;
+        } 
+        if (match.color == BLUE_TARGET) {
+            return ColorValue.BLUE;
+          } else if (match.color == RED_TARGET) {
+            return ColorValue.RED;
+          } else if (match.color == GREEN_TARGET) {
+            return ColorValue.GREEN;
+          } else if (match.color == YELLOW_TARGET) {
+            return ColorValue.YELLOW;
+          } else {
+              return ColorValue.UNKOWN;
+          }
+    }
+}
