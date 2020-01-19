a1ca3a90a52fab6bfec720df43c6502f09cf23bd
ohad <ohadze@yahoo.com>
2020-01-19 13:00:56 -0500

    Copy diagnostics from common repo

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -11,6 +11,7 @@ import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.robot.utils.diag.Diagnostics;
 import frc.robot.utils.logging.Logging;
 
 /**
@@ -23,6 +24,8 @@ public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
   private RobotContainer m_robotContainer;
 
+  private Diagnostics diagnostics;
+
   /**
    * This function is run when the robot is first started up and should be used for any
    * initialization code.
@@ -32,6 +35,7 @@ public class Robot extends TimedRobot {
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
     m_robotContainer = new RobotContainer();
+    diagnostics = new Diagnostics();
   }
 
   /**
@@ -116,6 +120,7 @@ public class Robot extends TimedRobot {
   public void testInit() {
     // Cancels all running commands at the start of test mode.
     CommandScheduler.getInstance().cancelAll();
+    diagnostics.reset();
   }
 
   /**
@@ -123,5 +128,6 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void testPeriodic() {
+    diagnostics.refresh();
   }
 }
