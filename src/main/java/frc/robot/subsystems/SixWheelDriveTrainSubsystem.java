bb2df43b774a2a6b6346cd9f72274e05b154426a
Ben-Krugman <benjamin.krugman@gmail.com>
2020-01-26 12:57:49 -0500

    fixed exception

--- src/main/java/frc/robot/subsystems/SixWheelDriveTrainSubsystem.java
+++ src/main/java/frc/robot/subsystems/SixWheelDriveTrainSubsystem.java
@@ -39,7 +39,7 @@ public class SixWheelDriveTrainSubsystem extends SubsystemBase {
     right1 = new WPI_TalonSRX(Constants.MOTOR_RIGHT1_ID);
     right2 = new WPI_TalonSRX(Constants.MOTOR_RIGHT2_ID);
     leftEncoder = new Encoder(Constants.DRIVE_ENCODER_LEFT_ID[0], Constants.DRIVE_ENCODER_LEFT_ID[1]);
-    rightEncoder = new Encoder(Constants.DRIVE_ENCODER_RIGHT_ID[0], Constants.DRIVE_ENCODER_LEFT_ID[1], true);
+    rightEncoder = new Encoder(Constants.DRIVE_ENCODER_RIGHT_ID[0], Constants.DRIVE_ENCODER_RIGHT_ID[1], true);
     navX = new AHRS(SPI.Port.kMXP);
 
     left2.set(ControlMode.Follower, Constants.MOTOR_LEFT1_ID);
