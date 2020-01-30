2a29ed476214cd71f885a424543693a56e6610cd
Ben-Krugman <benjamin.krugman@gmail.com>
2020-01-29 20:45:41 -0500

    basic logic for state machine (wont work)

---
+++ src/main/java/frc/robot/commands/conveyorbelt/M2M3Command.java
@@ -0,0 +1,53 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands.conveyorbelt;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.ConveyorSubsystem;
+import frc.robot.subsystems.ConveyorSubsystem.State;
+
+public class M2M3Command extends CommandBase {
+  private ConveyorSubsystem conveyorSubsystem;
+  private final double STAGER_SPEED = 0.5;
+  private final double CONVEYOR_SPEED = 0.5;
+  private State state;
+  /**
+   * Creates a new M2M3M4Command.
+   */
+  public M2M3Command(ConveyorSubsystem conveyorSubsystem) {
+    this.conveyorSubsystem = conveyorSubsystem;
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(conveyorSubsystem);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    state = conveyorSubsystem.getState();
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    conveyorSubsystem.moveStager(STAGER_SPEED);
+    conveyorSubsystem.moveConveyor(CONVEYOR_SPEED);
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    conveyorSubsystem.moveStager(0);
+    conveyorSubsystem.moveConveyor(0);
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return conveyorSubsystem.getState() == conveyorSubsystem.wantedState(state);
+  }
+}
---
+++ src/main/java/frc/robot/commands/conveyorbelt/M3Command.java
@@ -0,0 +1,52 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands.conveyorbelt;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.ConveyorSubsystem;
+import frc.robot.subsystems.ConveyorSubsystem.State;
+
+public class M3Command extends CommandBase {
+  private final double STAGER_SPEED = 0.5;
+  private ConveyorSubsystem conveyorSubsystem;
+  private State state;
+  /**
+   * Creates a new M3M4Command.
+   */
+  public M3Command(ConveyorSubsystem conveyorSubsystem) {
+    this.conveyorSubsystem = conveyorSubsystem;
+    addRequirements(conveyorSubsystem);
+
+
+    // Use addRequirements() here to declare subsystem dependencies.
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    state = conveyorSubsystem.getState();
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    conveyorSubsystem.moveStager(STAGER_SPEED);
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    conveyorSubsystem.moveStager(0);
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return conveyorSubsystem.getState() == conveyorSubsystem.wantedState(state);
+  }
+}
