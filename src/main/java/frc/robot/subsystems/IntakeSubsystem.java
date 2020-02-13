d3d61838286f2bbc399efecc0dffc96754971022
r <r@r.com>
2020-02-12 19:53:01 -0500

    Finished intake subsystem and command, Still needs to be tested

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -7,27 +7,39 @@
 
 package frc.robot.subsystems;
 
-import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
+import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 import frc.robot.utils.logging.Logging;
 
 public class IntakeSubsystem extends SubsystemBase {
   private WPI_TalonSRX intakeMotor;
+  private Solenoid piston;
+
   /**
    * Creates a new IntakeSubsystem.
    */
   public IntakeSubsystem() {
     intakeMotor = new WPI_TalonSRX(Constants.MOTOR_LEFT1_ID);
+    piston = new Solenoid(Constants.INTAKE_PISTON_ID);
+  
+    int TIMEOUT = 100;
+
+    intakeMotor.configNominalOutputForward(0, TIMEOUT);
+    intakeMotor.configNominalOutputReverse(0, TIMEOUT);
+    intakeMotor.configPeakOutputForward(1, TIMEOUT);
+    intakeMotor.configPeakOutputReverse(-1, TIMEOUT);
+    intakeMotor.setNeutralMode(NeutralMode.Brake);
   }
 
   public Logging.LoggingContext loggingContext = new Logging.LoggingContext(this.getClass()) {
 
     @Override
     protected void addAll() {
-      add("Applied Voltage to Intake Motor", intakeMotor.getMotorOutputVoltage());
+      add("Intake Motor Applied Voltage", intakeMotor.getMotorOutputVoltage());
     }
   };
 
@@ -38,14 +50,16 @@ public class IntakeSubsystem extends SubsystemBase {
 
   public void deployPiston() {
     System.out.println("deploy");
+    piston.set(true);
   }
-  
+
   public void retractPiston() {
     System.out.println("retract");
+    piston.set(false);
   }
 
   public void spinMotor(double speed){
     System.out.println("spin");
+    intakeMotor.set(speed);
   }
-
 }
