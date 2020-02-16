7da8749c9e99e1ac328b00320eda166a2a674a17
Team 4048 <timofey.tylik@gmail.com>
2020-02-16 15:26:40 -0500

    Fixed pull comments

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -35,12 +35,7 @@ public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
   private RobotContainer m_robotContainer;
   private Diagnostics diagnostics;
-  private DigitalInput sensor1;
-  private DigitalInput sensor2;
-  private DigitalInput sensor3;
-  private DigitalInput sensor4;
-  private DigitalInput sensor5;
-  private DigitalInput sensor6;
+
 
   /**
    * This function is run when the robot is first started up and should be used for any
