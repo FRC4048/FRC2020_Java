7620a3751d0290d8a66d0b79074cc96db550405a
Ben-Krugman <benjamin.krugman@gmail.com>
2020-02-20 11:52:46 -0500

    camera is bac

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -51,7 +51,7 @@ public class Robot extends TimedRobot {
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
     diagnostics = new Diagnostics();
-    // CameraServer.getInstance().startAutomaticCapture();
+    CameraServer.getInstance().startAutomaticCapture();
     m_robotContainer = new RobotContainer();
   }
 
