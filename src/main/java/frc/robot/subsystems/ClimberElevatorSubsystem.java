43d8b6e9b675c92946d413228e0543e1ffbb3bfe
Ben-Krugman <benjamin.krugman@gmail.com>
2020-02-19 19:59:12 -0500

    put back inversion

--- src/main/java/frc/robot/subsystems/ClimberElevatorSubsystem.java
+++ src/main/java/frc/robot/subsystems/ClimberElevatorSubsystem.java
@@ -39,6 +39,7 @@ public class ClimberElevatorSubsystem extends SubsystemBase {
     climberMotor.configPeakOutputForward(1, TIMEOUT);
     climberMotor.configPeakOutputReverse(-1, TIMEOUT);
     climberMotor.setNeutralMode(NeutralMode.Brake);
+    climberMotor.setInverted(true);
     
     climberMotor.configForwardLimitSwitchSource(LimitSwitchSource.FeedbackConnector, LimitSwitchNormal.NormallyOpen);
     climberMotor.configReverseLimitSwitchSource(LimitSwitchSource.FeedbackConnector, LimitSwitchNormal.NormallyOpen);
