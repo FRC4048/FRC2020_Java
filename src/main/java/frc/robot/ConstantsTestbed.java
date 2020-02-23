6ac59515aa9bcc45c57619cf263b3acf7fd9f596
Ben-Krugman <benjamin.krugman@gmail.com>
2020-02-23 12:06:34 -0500

    Cp fix 2 part 2 the series

--- src/main/java/frc/robot/ConstantsTestbed.java
+++ src/main/java/frc/robot/ConstantsTestbed.java
@@ -14,7 +14,7 @@ import edu.wpi.first.wpilibj.kinematics.DifferentialDriveKinematics;
  */
 interface ConstantsTestbed {
     //Global Constant
-    public static final boolean ENABLE_DEBUG = false;
+    public static final boolean ENABLE_DEBUG = true;
 
     //CAN ID
     public static final int PDP_CAN_ID = 0;
