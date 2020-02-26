6f6e7a62596cf2422ea75110fe709b6b07a74e05
Timofey-Tylik <36483466+Timofey-Tylik@users.noreply.github.com>
2020-02-26 18:35:09 -0500

    Update Robot.java

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -154,7 +154,6 @@ public class Robot extends TimedRobot {
     if (m_autonomousCommand != null) {
       m_autonomousCommand.cancel();
     }
-    m_robotContainer.driveTrainGetter().resetGyro();
   }
 
   /**
